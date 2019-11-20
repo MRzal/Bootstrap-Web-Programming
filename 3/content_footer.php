72e0a87284de24bfc8adeeb2b0204f8f4c82ae8f
MRzal <38412845+MRzal@users.noreply.github.com>
2019-11-20 20:14:22 +0700

    Add files via upload

---
+++ 3/content_footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <center> 
+      <span style="font-size: 12px"><strong style="text-align: center; color: #2CC115;font-size: 18px;">Copyright &copy; <?php echo date("Y") ?> Sistem Akademik and Created By<br> 
+      <a href="https://www.facebook.com/derryikhsan.sholahuddin1" style="text-align: center; color: #156EC1;">Dany Pradana 4611417021, M Rizal Al Qodri 4611417027,Ahmad Fawaid 4611417029, and Teguh Pamungkas 461141740</a></strong></span>
+    </center>
+</footer>
---
+++ 3/dt_jadwal.php
@@ -0,0 +1,43 @@
+				<thead>
+					<tr>
+						<th>Mata Pelajaran</th>
+						<th>Guru</th>
+						<th>Ruangan</th>
+						<th>Hari</th>
+						<th>Jam</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						$querymhs = mysqli_query($konek, "SELECT Ekstrakurikuler, Kode_Jurusan, Nama_Ekstrakurikuler FROM Siswa INNER JOIN Ekstrakurikuler ON Ekstrakurikuler=Kode_Jurusan WHERE NIM='$_SESSION[Username]'");
+						if($querymhs == false){
+							die("Terdapat Kesalahan : ". mysqli_error($konek));
+						}
+						while ($mhs = mysqli_fetch_array($querymhs)){
+							
+							$queryjadwal = mysqli_query ($konek, "SELECT Id_Jadwal, Kode_Matakuliah_Jadwal, NIP_Jadwal, Kode_Ruangan_Jadwal, Hari,
+							Jam, Kode_Matakuliah, Nama_Matakuliah, NIP, Nama_Guru, Kode_Ruangan, Nama_Ruangan, Kode_Jurusan, Nama_Ekstrakurikuler FROM jadwal 
+							INNER JOIN Matapelajaran ON Kode_Matakuliah_Jadwal=Kode_Matakuliah
+							INNER JOIN Guru ON NIP_Jadwal=NIP
+							INNER JOIN ruangan ON Kode_Ruangan_Jadwal=Kode_Ruangan
+							INNER JOIN Ekstrakurikuler ON Kode_Jurusan_Jadwal=Kode_Jurusan WHERE Kode_Jurusan_Jadwal='$mhs[Ekstrakurikuler]'");
+							if($queryjadwal == false){
+								die ("Terjadi Kesalahan : ". mysqli_error($konek));
+							}
+							while ($jadwal = mysqli_fetch_array ($queryjadwal)){
+							
+								echo "
+									<tr>
+										<td>$jadwal[Nama_Matakuliah]</td>
+										<td>$jadwal[Nama_Guru]</td>
+										<td>$jadwal[Nama_Ruangan]</td>
+										<td>$jadwal[Hari]</td>
+										<td>$jadwal[Jam]</td>
+									</tr>";
+							}
+							
+						}
+					
+						
+					?>
+				</tbody>
